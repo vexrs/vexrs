952fb2124cc9ae0c981d1da60ae42497a20056be
Riley Wilton <riley.j.wilton@gmail.com>
2022-04-22 14:37:23 -0400

    Moving the panic handler from the template to here.

--- src/panic.rs
+++ src/panic.rs
@@ -1,1 +1,19 @@
 use ceros_serial::{protocol::{DataType, CEROSSerial}, serial::Serial};
+
+
+
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    
+    let mut data = format!("\x07\x1b[1;31mPANIC: {}\x1b[0m\n", info);
+
+    let mut serial_port = Serial::new();
+    let mut serial = CEROSSerial::new(&mut serial_port);
+    serial.write_data(DataType::Print, data.as_bytes().to_vec());
+    
+    // Block for 50 ms to wait for the serial data to go through
+    ceros_core::util::block(50);
+
+    loop {}
+}
