9305f54f4ad9b6f417447bb7a0e1f99c4dad03e9
Riley Wilton <riley.j.wilton@gmail.com>
2022-04-21 23:04:41 -0400

    And the unsafe blocks.

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,6 @@
 
 pub fn block(ms: u32) {
-    let time = vexv5rt::vexSystemTimeGet();
-    while vexv5rt::vexSystemTimeGet() - time < ms {
+    let time = unsafe{vexv5rt::vexSystemTimeGet()};
+    while unsafe{vexv5rt::vexSystemTimeGet()} - time < ms {
     }
 }
