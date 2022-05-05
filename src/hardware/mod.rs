0f847ce942a2e718f4e1baa9876c8cebc84003ac
Riley Wilton <riley.j.wilton@gmail.com>
2022-05-05 15:11:26 -0400

    Add module for devices.

--- src/hardware/mod.rs
+++ src/hardware/mod.rs
@@ -11,9 +11,14 @@ pub mod controller;
 // Utilities for getting the competition status
 pub mod competition;
 
-// Basic hardware abstraction on top of the v5 brain.
+// Basic hardware abstraction on top of the v5 brain display.
 #[cfg(feature="graphics")]
 pub mod display;
+
+// Structures to manage devices plugged into smart and three-wire ports.
+pub mod devices;
+
+
 
 // Utilities
 pub mod util;
