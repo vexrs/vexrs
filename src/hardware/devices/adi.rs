51240a828456f73bafcfa06b9baa456634ffc2fa
Riley Wilton <riley.j.wilton@gmail.com>
2022-05-06 12:28:12 -0400

    Tidied up the ADI Digital in device.

--- src/hardware/devices/adi.rs
+++ src/hardware/devices/adi.rs
@@ -38,30 +38,30 @@ fn get_adi_value(device: vexv5rt::V5_DeviceT, port: u32) -> i32 {
 #[derive(Copy, Clone)]
 pub struct ADIDigitalIn {
     /// The port number of this device
-    port_number: u32,
-    /// The ADI port that this device is connected to
     port: u32,
+    /// The ADI port that this device is connected to
+    index: u32,
 }
 
 impl ADIDigitalIn {
     /// Creates a new ADI digital in device
-    pub fn new(port_number: u32, port: u32) -> Self {
+    pub fn new(port: u32, index: u32) -> Self {
         ADIDigitalIn {
-            port_number,
             port,
+            index,
         }
     }
 
     /// Reads from the ADI digital in device
-    pub fn read(&self) -> i32 {
+    pub fn read(&self) -> bool {
         // Lock the mutex for the port
-        let _mtx = get_device_manager().unwrap().lock_adi_device(self.port_number, self.port, ADIPort::DigitalIn);
+        let _mtx = get_device_manager().unwrap().lock_adi_device(self.port, self.index, ADIPort::DigitalIn);
 
         // Read the value
-        let value = get_adi_value(self.get_vex_device(), self.port);
+        let value = get_adi_value(self.get_vex_device(), self.index);
 
         // Return it
-        value
+        value != 0
     }
 }
 
@@ -69,7 +69,7 @@ impl Device for ADIDigitalIn {
 
     fn init(&mut self) {
         // Configure the port to be digital in
-        set_adi_config(self.get_vex_device(), self.port, ADIPort::DigitalIn);
+        set_adi_config(self.get_vex_device(), self.index, ADIPort::DigitalIn);
     }
 
     fn calibrate(&mut self) {
@@ -82,17 +82,17 @@ impl Device for ADIDigitalIn {
     }
 
     fn get_port_number(&self) -> u32 {
-        self.port_number
+        self.port
     }
 }
 
-
 impl ADIDevice for ADIDigitalIn {
     fn new_adi(port: u32, index: u32) -> Self {
-        ADIDigitalIn { port_number: port, port: index }
+        ADIDigitalIn { port, index }
     }
 
     fn get_adi_port(&self) -> ADIPort {
         ADIPort::DigitalIn
     }
 }
+
