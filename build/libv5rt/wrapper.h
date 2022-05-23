a18a1683916949c114d12c25f39e7c75151dc8fa
Riley Wilton <riley.j.wilton@gmail.com>
2022-05-23 12:20:56 -0400

    Successful build and link to libv5rt.

---
+++ build/libv5rt/wrapper.h
@@ -0,0 +1,4 @@
+// Use custom stdint.h from c99
+#include "stdint.h"
+
+#include <v5.h>
---
+++ src/libv5rt.rs
@@ -0,0 +1,10 @@
+// Generated bindings for libv5rt
+
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+#![no_std]
+
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
