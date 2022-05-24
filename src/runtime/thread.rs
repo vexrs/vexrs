4221d3daa14bb26eef8519a53f59d3347b42c722
Riley Wilton <riley.j.wilton@gmail.com>
2022-05-24 12:35:58 -0400

    Fixed the error. The offset was being set as an offset of bytes not of usizes.

--- src/runtime/thread.rs
+++ src/runtime/thread.rs
@@ -96,7 +96,8 @@ impl Thread {
             "push {{{1}}}", // Push the end label as the saved program counter
             "push {{{0}}}", // Push the guard function as the link register (this should be overwritten when a function returns)
             "push {{r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12}}", // Push the general purpose registers
-            "sub {4}, sp", // Convert the current stack pointer as an offset
+            "sub {4}, sp", // Convert the current stack pointer to an offset 
+            "lsr {4}, 2", // Divide the offset by four in order to get the offset in usizes.
             "str {4}, [{2}]", // Save the stack offset
             "mov sp, {3}", // Load the new stack pointer (we are now on a new stack! yay!
             "pop {{r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12}}", // Pop the general purpose registers
